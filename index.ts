ab3ce61dc3b3580d4725099062ae3e995e99a12a
kimjust6 <mletemps@hotmail.com>
2024-12-01 12:57:02 -0500

    added prettier

--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+async function parse() {
+    const path = "input/input.txt";
+    const myFile = Bun.file(path);
+
+    const myStream = await myFile.text();
+    const decoder = new TextDecoder();
+
+    let arrayLeft = [];
+    let arrayRight = [];
+
+    for (const chunk of myStream.split("\n")) {
+        const arr = chunk.split("\\s+");
+        arrayLeft.push(arr[0]);
+        arrayRight.push(arr[1]);
+    }
+}
+
+async function main() {
+    await parse();
+}
+
+main();
