b761251bc420164ccba515d9f60527ecfcce1beb
kimjust6 <mletemps@hotmail.com>
2024-12-01 13:52:44 -0500

    finished part1

--- index.ts
+++ index.ts
@@ -1,31 +1,59 @@
-async function parse(): Promise<[Map<number, number>, Map<number, number>]> {
+async function parse(): Promise<[number[], number[]]> {
     const path = "input/input.txt";
     const myFile = Bun.file(path);
 
     const myStream = await myFile.text();
-    const decoder = new TextDecoder();
+    // const decoder = new TextDecoder();
 
-    // let arrayLeft = [];
-    // let arrayRight = [];
-
-    let leftMap = new Map<number, number>();
-    let rightMap = new Map<number, number>();
+    let leftArray: number[] = [];
+    let rightArray: number[] = [];
 
     for (const chunk of myStream.split("\n")) {
-        const arr = chunk.split("\\s+");
-        const leftNumber = parseInt(arr[0]);
-        const rightNumber = parseInt(arr[1]);
+        const arr = chunk.split(/\s+/);
 
-        leftMap.set(leftNumber, leftMap.get(leftNumber) ?? 0 + 1);
-        rightMap.set(rightNumber, rightMap.get(rightNumber) ?? 0 + 1);
+        const leftNumber = parseInt(arr[0]);
+
+        const rightNumber = parseInt(arr[1]);
+        if (!isNaN(leftNumber)) {
+            leftArray.push(leftNumber);
+        }
+        if (!isNaN(rightNumber)) {
+            rightArray.push(rightNumber);
+        }
+    }
+    return [leftArray, rightArray];
+}
+
+function getSmallestNumberIndex(array: number[]): number {
+    let smallestNumber = array[0];
+
+    let i = 0;
+    let returnIndex = 0;
+    for (; i < array.length; i++) {
+        if (array[i] < smallestNumber) {
+            smallestNumber = array[i];
+            returnIndex = i;
+        }
     }
 
-    return [leftMap, rightMap];
+    return returnIndex;
 }
 
 async function main() {
-    const [leftMap, rightMap] = await parse();
+    let [leftArray, rightArray] = await parse();
 
+    let difference = 0;
+    // console.log(getSmallestNumberIndex(rightArray));
+    while (leftArray.length > 0 && rightArray.length > 0) {
+        let leftIndex = getSmallestNumberIndex(leftArray);
+        let rightIndex = getSmallestNumberIndex(rightArray);
+
+        difference += Math.abs(leftArray[leftIndex] - rightArray[rightIndex]);
+        leftArray.splice(leftIndex, 1);
+        rightArray.splice(rightIndex, 1);
+    }
+
+    console.log(difference);
 }
 
 main();
