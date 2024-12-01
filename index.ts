f8c79cb9d071510a77c5c26f272a4c7a619ad0a5
kimjust6 <mletemps@hotmail.com>
2024-12-01 13:14:59 -0500

    update before revert

--- index.ts
+++ index.ts
@@ -1,22 +1,31 @@
-async function parse() {
+async function parse(): Promise<[Map<number, number>, Map<number, number>]> {
     const path = "input/input.txt";
     const myFile = Bun.file(path);
 
     const myStream = await myFile.text();
     const decoder = new TextDecoder();
 
-    let arrayLeft = [];
-    let arrayRight = [];
+    // let arrayLeft = [];
+    // let arrayRight = [];
+
+    let leftMap = new Map<number, number>();
+    let rightMap = new Map<number, number>();
 
     for (const chunk of myStream.split("\n")) {
         const arr = chunk.split("\\s+");
-        arrayLeft.push(arr[0]);
-        arrayRight.push(arr[1]);
+        const leftNumber = parseInt(arr[0]);
+        const rightNumber = parseInt(arr[1]);
+
+        leftMap.set(leftNumber, leftMap.get(leftNumber) ?? 0 + 1);
+        rightMap.set(rightNumber, rightMap.get(rightNumber) ?? 0 + 1);
     }
+
+    return [leftMap, rightMap];
 }
 
 async function main() {
-    await parse();
+    const [leftMap, rightMap] = await parse();
+
 }
 
 main();
