b7695f77f5685bfc49ec11f54443945eb6339abd
kimjust6 <mletemps@hotmail.com>
2024-12-02 01:24:04 -0500

    finished part1

--- day2.ts
+++ day2.ts
@@ -1,24 +1,51 @@
-async function parse(): Promise<[number[], number[]]> {
-    const path = "input/day2.txt";
+async function parse(path: string): Promise<number[][]> {
     const myFile = Bun.file(path);
 
     const myStream = await myFile.text();
 
-    let leftArray: number[] = [];
-    let rightArray: number[] = [];
+    let arrayOfArray: number[][] = [];
 
     for (const chunk of myStream.split("\n")) {
         const arr = chunk.split(/\s+/);
-
-        const leftNumber = parseInt(arr[0]);
-
-        const rightNumber = parseInt(arr[1]);
-        if (!isNaN(leftNumber)) {
-            leftArray.push(leftNumber);
+        const row = [];
+        for (let i = 0; i < arr.length; i++) {
+            const asNumber = parseInt(arr[i]);
+            row.push(asNumber);
         }
-        if (!isNaN(rightNumber)) {
-            rightArray.push(rightNumber);
+        arrayOfArray.push(row);
+    }
+    return arrayOfArray;
+}
+
+async function partOne() {
+    const myArray = await parse("input/day2.txt");
+
+    let safeCount = 0;
+    for (let i = 0; i < myArray.length; ++i) {
+        let lastDiff: number = 0;
+        let isSafe = true;
+        for (let j = 1; j < myArray[i].length && isSafe; ++j) {
+            let num1 = myArray[i][j],
+                num2 = myArray[i][j - 1],
+                currentDiff = num1 - num2;
+            if (
+                Math.abs(currentDiff) > 3 ||
+                currentDiff === 0 ||
+                currentDiff * lastDiff < 0
+            ) {
+                isSafe = false;
+            }
+            lastDiff = currentDiff;
+        }
+        if (isSafe) {
+            safeCount++;
         }
     }
-    return [leftArray, rightArray];
+    console.log(safeCount);
 }
+
+async function main() {
+    partOne();
+}
+
+main();
