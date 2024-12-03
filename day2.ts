e9b5f281fda8888f052acae6af6f66d1996c9a90
kimjust6 <mletemps@hotmail.com>
2024-12-03 00:32:24 -0500

    intermediate commit

--- day2.ts
+++ day2.ts
@@ -22,9 +22,41 @@ async function partOne() {
 
     let safeCount = 0;
     for (let i = 0; i < myArray.length; ++i) {
+        let isRowSafe = isSafe(myArray[i]);
+        if (isRowSafe) {
+            safeCount++;
+        }
+    }
+    console.log(safeCount);
+}
+
+function isSafe(myArray: number[]): boolean {
+    let lastDiff: number = 0;
+    let isSafe = true;
+    for (let j = 1; j < myArray.length && isSafe; ++j) {
+        let num1 = myArray[j],
+            num2 = myArray[j - 1],
+            currentDiff = num1 - num2;
+        if (
+            Math.abs(currentDiff) > 3 ||
+            currentDiff === 0 ||
+            currentDiff * lastDiff < 0
+        ) {
+            isSafe = false;
+        }
+        lastDiff = currentDiff;
+    }
+    return isSafe;
+}
+
+async function partTwo() {
+    const myArray = await parse("input/day2.txt");
+
+    let safeCount = 0;
+    for (let i = 0; i < myArray.length; ++i) {
         let lastDiff: number = 0;
-        let isSafe = true;
-        for (let j = 1; j < myArray[i].length && isSafe; ++j) {
+        let isRowSafe = true;
+        for (let j = 1; j < myArray[i].length && isRowSafe; ++j) {
             let num1 = myArray[i][j],
                 num2 = myArray[i][j - 1],
                 currentDiff = num1 - num2;
@@ -33,11 +65,17 @@ async function partOne() {
                 currentDiff === 0 ||
                 currentDiff * lastDiff < 0
             ) {
-                isSafe = false;
+                let myArray2 = [...myArray[i]];
+                let myArray3 = [...myArray[i]];
+                myArray[i].splice(j - 1, 1);
+                myArray2.splice(j, 1);
+                // myArray3.splice(j + 1, 1);
+                isRowSafe =
+                    isSafe(myArray[i]) || isSafe(myArray2) || isSafe(myArray3);
             }
             lastDiff = currentDiff;
         }
-        if (isSafe) {
+        if (isRowSafe) {
             safeCount++;
         }
     }
@@ -45,7 +83,8 @@ async function partOne() {
 }
 
 async function main() {
-    partOne();
+    // partOne();
+    partTwo();
 }
 
 main();
